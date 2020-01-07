1d014cd24e76080467def35c388e5d6dcacca013
Jacob Jang <mcppaint@gmail.com>
2020-01-07 09:50:47 +0900

    Create get_build_count_for_each_job.sh

---
+++ jenkins/get_build_count_for_each_job.sh
@@ -0,0 +1,14 @@
+# Input your Jenkins domain
+domain=https://
+# Input directory with Jenkins jobs
+job_dir=/mcpaint/jenkins_home/jobs
+
+IFS=$'\n' jobs=(`ls -1 $job_dir`)
+
+for job in "${jobs[@]}"; do
+  build_dir=$job_dir/$job/builds
+  encoded_job=$(python -c "import urllib; print urllib.quote('''$job''')")
+  if test -d $build_dir
+    then echo -e "$(ls -A1 $build_dir | grep -v last | grep -v legacyIds | wc -l) $job\t$domain/job/$encoded_job"
+  fi
+done
